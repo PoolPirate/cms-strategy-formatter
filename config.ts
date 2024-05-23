30ef877bd3d805d8db2be2a8afcf7df2fb0d087e
Playwo <eliaswolf1609@gmail.com>
2024-05-23 12:59:44 +0200

    feat: update spreadsheet url to levana drive

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 //Link to risk report spreadsheet
 export const reportCardUrl =
-  "https://docs.google.com/spreadsheets/d/14Uyvia9FRPZ7R1ayz5N0kzFkF9dCi0t9CpFXs3NW2yA/edit?usp=sharing";
+  "https://docs.google.com/spreadsheets/d/1RDRyC-beyLWnOwFkIIn2IzO54mOaVwtU37jNnm1kusg";
 //Levana factory address
 export const factoryAddress =
   "osmo1ssw6x553kzqher0earlkwlxasfm2stnl3ms3ma2zz4tnajxyyaaqlucd45";
